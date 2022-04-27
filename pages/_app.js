f3a6352c12cbe37e681179227bc0140bcfde3b10
Alfredo Baldoceda <alfredo.baldoceda@nomissolutions.com>
2022-04-27 16:07:08 -0400

    New Changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
-import '../styles/globals.css'
+import Link from "next/link";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div className="max-h-fit bg-white dark:bg-slate-900 text-slate-900 dark:text-slate-400 text-base font-medium">
+            <div className="mx-auto w-9/12 tracking-tight">
+                <header>
+                    <nav className="pt-2">
+                        <ul className="flex flex-row justify-center space-x-4">
+                            <li>
+                                <Link href="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link href="/about">About</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+                <Component {...pageProps} />
+            </div>
+        </div>
+    );
 }
 
-export default MyApp
+export default MyApp;
